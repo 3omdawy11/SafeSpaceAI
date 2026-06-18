9792027b999cbae0d9d1695112e94996d4497694
3omdawy11 <mohamedzaky970@gmail.com>
2026-06-19 01:51:00 +0300

    add localhost:8080 to CORS origins

--- src/api/app.py
+++ src/api/app.py
@@ -49,8 +49,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://moemad07-safespace-ai-frontend.hf.space",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
